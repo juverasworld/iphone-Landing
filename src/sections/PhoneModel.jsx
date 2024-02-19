0896b2fdfeb17daef756f307f479a87723b0ebc1
Vee <ifebuchejulia@gmail.com>
2024-02-19 18:03:33 +0100

    About to solve a huge blocker

--- src/sections/PhoneModel.jsx
+++ src/sections/PhoneModel.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import Model from "../assets/3D-Model/scene";
 
 const Container = styled.div`
   width: 100vw;
@@ -10,7 +11,7 @@ const Container = styled.div`
   position: fixed;
   top: 0;
   z-index: 1;
-  background-color: transparent;
+  background-color: transparent;  
 `;
 
 const PhoneModel = () => {
@@ -19,10 +20,7 @@ const PhoneModel = () => {
       <Canvas>
         <ambientLight intensity={1.25}/>
         <directionalLight position={[1,0,0]}/>
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial color="red" />
-        </mesh>
+        <Model/>
 
         <OrbitControls />
       </Canvas>
