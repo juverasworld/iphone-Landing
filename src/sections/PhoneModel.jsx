cc0a0b57f05f06bfca97e586892fe2d0e92c3fe7
Vee <ifebuchejulia@gmail.com>
2024-02-19 17:31:59 +0100

    included the phone model section

--- src/sections/PhoneModel.jsx
+++ src/sections/PhoneModel.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Canvas } from '@react-three/fiber';
+import React from "react";
+import styled from "styled-components";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
 const Container = styled.div`
-width:100vw;
-height:100vh;
+  width: 100vw;
+  height: 100vh;
 
-position:fixed;
-top:0;
-z-index: 1;
-background-color:transparent;
-
-`
-
+  position: fixed;
+  top: 0;
+  z-index: 1;
+  background-color: transparent;
+`;
 
 const PhoneModel = () => {
   return (
-   <Container>
-    <Canvas>
+    <Container>
+      <Canvas>
+        <ambientLight intensity={1.25}/>
+        <directionalLight position={[1,0,0]}/>
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
 
-    </Canvas>
-   </Container>
-  )
-}
+        <OrbitControls />
+      </Canvas>
+    </Container>
+  );
+};
 
-export default PhoneModel
+export default PhoneModel;
