4226823c27e5a7f078352a4700c87b065b80c1c2
Vee <ifebuchejulia@gmail.com>
2024-02-20 05:58:49 +0100

    completed the corrections on design correction, it was only a scrub changing it to 1 instead of true

--- src/sections/DesignSection.jsx
+++ src/sections/DesignSection.jsx
@@ -1,6 +1,9 @@
-import React, { useRef, useLayoutEffect } from "react";
-import styled from "styled-components";
 import gsap from "gsap";
+import React from "react";
+import { useLayoutEffect } from "react";
+import { useRef } from "react";
+import styled from "styled-components";
+
 const Section = styled.section`
   width: 100vw;
   height: 100vh;
@@ -10,14 +13,15 @@ const Section = styled.section`
   flex-direction: column;
   justify-content: flex-end;
   align-items: center;
+
   background-color: var(--white);
   overflow: hidden;
 `;
-
 const TextContainer = styled.p`
   width: 100%;
   height: 50vh;
   z-index: 1;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -29,6 +33,22 @@ const TextContainer = styled.p`
     width: 90%;
     font-weight: 600;
     text-transform: capitalize;
+  }
+
+  @media screen and (max-width: 70em) {
+    span {
+      font-size: var(--fontxxxl);
+    }
+  }
+  @media screen and (max-width: 64em) {
+    span {
+      font-size: var(--fontxxl);
+    }
+  }
+  @media screen and (max-width: 48em) {
+    span {
+      font-size: var(--fontlg);
+    }
   }
 `;
 const TextContainer2 = styled.p`
@@ -42,13 +62,31 @@ const TextContainer2 = styled.p`
   color: var(--dark);
 
   span {
+    font-size: var(--fontxxxl);
     width: 80%;
     font-weight: 600;
     text-transform: capitalize;
+    align-self: flex-end;
+    text-align: right;
+  }
 
-    font-size: var(--fontxxxl);
+  @media screen and (max-width: 70em) {
+    span {
+      font-size: var(--fontxxl);
+    }
+  }
+  @media screen and (max-width: 64em) {
+    span {
+      font-size: var(--fontxl);
+    }
+  }
+  @media screen and (max-width: 48em) {
+    span {
+      font-size: var(--fontlg);
+    }
   }
 `;
+
 const DesignSection = () => {
   const container = useRef(null);
   const textOne = useRef(null);
@@ -60,9 +98,8 @@ const DesignSection = () => {
         scrollTrigger: {
           trigger: container.current,
           start: "top-=500 top",
-          end: " bottom top",
-          // markers:true,
-          scrub: true,
+          end: "bottom top",
+          scrub: 1,
         },
       })
       .fromTo(textOne.current, { x: 0 }, { x: "10%" }, "key1")
@@ -72,13 +109,15 @@ const DesignSection = () => {
       if (t1) t1.kill();
     };
   }, []);
+
   return (
     <Section ref={container}>
       <TextContainer ref={textOne}>
-        <span>Flaw-less design with stong durability.</span>
+        <span>Flaw-less design with strong durability.</span>
       </TextContainer>
+
       <TextContainer2 ref={textTwo}>
-        <span>Flat-edge design with toughhest smartphone.</span>
+        <span>Flat-edge design with toughest smartphone glass.</span>
       </TextContainer2>
     </Section>
   );
